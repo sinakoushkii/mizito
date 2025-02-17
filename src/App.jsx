e6333cfbf269c88b6d16414b10732a4df4f50e6f
sina koushki <sinaderik7@gmail.com>
2025-02-17 11:35:40 +0330

    mizito project is created with persian configuration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">این یک میز کار است</h2>
+    </div>
+  );
+};
+
+export default App;
