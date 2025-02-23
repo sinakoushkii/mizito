f1e7edb2fdac7d4ae715b276c924fd1c98df3955
sina koushki <sinaderik7@gmail.com>
2025-02-23 15:00:17 +0330

    padding is added to the bottom od donut chart

--- src/components/DonutChart.jsx
+++ src/components/DonutChart.jsx
@@ -11,7 +11,7 @@ export const DonutChart = () => {
       },
       labels: ["شنبه", "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه"], // Custom labels
       legend: {
-        position: "bottom",  // Moves labels under the chart
+        position: "bottom", // Moves labels under the chart
         horizontalAlign: "center", // Centers them
       },
       responsive: [
@@ -31,18 +31,20 @@ export const DonutChart = () => {
   });
 
   return (
-    <div className="w-full h-[100px] pb-10 mb-5" id="chart">
+    <div className="w-full h-[100px] pb-10 mb-5">
       <p
         className={`w-full px-2 py-1 ${getNavbarBackground()} text-white text-[13px]`}
       >
         بیشترین فعالیت شما
       </p>
-      <ReactApexChart
-        options={state.options}
-        series={state.series}
-        type="donut"
-        height={250}
-      />
+      <div className="pb-[5rem] pt-[1rem]">
+        <ReactApexChart
+          options={state.options}
+          series={state.series}
+          type="donut"
+          height={250}
+        />
+      </div>
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,9 +17,9 @@ const Sidebar = ({ isSidebarOpen, navbarHeight }) => {
 
   return (
     <div
-      className={`fixed top-[${navbarHeight}px] right-0 ${ isSidebarOpen ? "translate-x-0" : "translate-x-full"} bg-white lg:static lg:block shadow-xl w-[270px] px-1 py-2 transform transition-transform duration-300 z-20 h-screen border-2 overflow-scroll`}
+      className={`fixed top-[${navbarHeight}px] right-0 ${ isSidebarOpen ? "translate-x-0" : "translate-x-full"} bg-white lg:static lg:block shadow-xl w-[270px] px-1 py-2 transform transition-transform duration-300 z-20 h-screen border-2`}
     >
-      <div className="flex flex-col items-start justify-start gap-2 h-full w-full py-4 overflow-scroll">
+      <div className="flex flex-col items-start justify-start gap-2 h-full w-full pt-4 pb-12 overflow-scroll">
         {sidebarLinks.map((link) => (
           <Link
             className={`flex items-center gap-2 text-lg text-gray-700 hover:bg-gray-300 w-full ${
@@ -41,6 +41,7 @@ const Sidebar = ({ isSidebarOpen, navbarHeight }) => {
         ))}
         <Colleagues />
         <DonutChart />
+
         <div className="grow">
 
         </div>
